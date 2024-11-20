76e412ed55081f70855c7040a839bec770301882
willcommits <pmambambo2@gmail.com>
2024-11-20 15:00:44 +0200

    commit

--- obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25517bc3f45ce5ba1b052d52221240099072ef8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2183b0339e75002df69244abb59ed05a7975922d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
